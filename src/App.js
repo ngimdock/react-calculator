530bb924b182b60e2e600333619bd7940695f94d
ngimdock <ngimdockzemfack@gmail.com>
2021-07-14 09:56:41 +0100

    version static de ma calculatrice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [affichage, setAffichage] = useState("hey");
+  const [operande, setOperande] = useState("");
+  const [operateur, setOperateur] = useState("+");
+
+  function handleChange(chaine){
+    console.log(affichage);
+    setAffichage(affichage + " " + chaine);
+    console.log(affichage);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>POUR MON PROGRAMME HELLO WORLD J'AI DÉCIDE DE FAIRE UNE CALCULATRICE BASIQUE</h3>
+
+      <section>
+        <div className="result-block"> <span>{affichage}</span> </div>
+        <div className="num-block">
+          <div className="items">
+            <div className="big" onClick={() => setOperande("")}>Clear</div>
+            <div className="small">C</div>
+            <div className="small"> ÷ </div>
+          </div>
+
+          <div className="items">
+            <div className="small" onClick={() => handleChange("7")}>7</div>
+            <div className="small">8</div>
+            <div className="small">9</div>
+            <div className="small">×</div>
+          </div>
+
+          <div className="items">
+            <div className="small">4</div>
+            <div className="small">5</div>
+            <div className="small">6</div>
+            <div className="small">-</div>
+          </div>
+
+          <div className="items">
+            <div className="small">1</div>
+            <div className="small">2</div>
+            <div className="small">3</div>
+            <div className="small">+</div>
+          </div>
+
+          <div className="items">
+            <div className="small">0</div>
+            <div className="small">.</div>
+            <div className="big">=</div>
+          </div>
+        </div>
+      </section>
+
+      
+    </>
   );
 }
 
